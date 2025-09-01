271666968de340cb0504c02653aa70d0d08bdef7
Zedex01 <77065621+Zedex01@users.noreply.github.com>
2025-09-01 13:58:23 -0400

    Working Logger and setup

---
+++ src/main/java/net/matt/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.matt.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
