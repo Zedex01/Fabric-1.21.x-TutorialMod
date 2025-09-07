710d7a6329a4a113e9455fe3d931f0d16b52569e
Zedex01 <77065621+Zedex01@users.noreply.github.com>
2025-09-06 23:17:09 -0400

    Ep 23, Events

--- src/main/java/net/matt/tutorialmod/TutorialMod.java
+++ src/main/java/net/matt/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.matt.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.matt.tutorialmod.block.ModBlocks;
@@ -9,6 +10,13 @@ import net.matt.tutorialmod.component.ModDataComponentTypes;
 import net.matt.tutorialmod.item.ModItemGroups;
 import net.matt.tutorialmod.item.ModItems;
 import net.matt.tutorialmod.util.HammerUsageEvent;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.item.Items;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,5 +41,20 @@ public class TutorialMod implements ModInitializer {
 
         //Register event
         PlayerBlockBreakEvents.BEFORE.register(new HammerUsageEvent());
+
+        //Using Event
+        AttackEntityCallback.EVENT.register((player, world, hand, entity, hitResult)-> {
+            if(entity instanceof SheepEntity sheepEntity && !world.isClient()){
+                if(player.getMainHandStack().getItem() == Items.END_ROD){
+                    player.sendMessage(Text.literal("The player just hit a sheep with an end rod you sick Freak!"));
+                    player.getMainHandStack().decrement(1); //remove 1 count from the stack item
+                    sheepEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 600, 6));
+                }
+
+                return ActionResult.PASS;
+            }
+            return ActionResult.PASS;
+        });
+
 	}
 }
